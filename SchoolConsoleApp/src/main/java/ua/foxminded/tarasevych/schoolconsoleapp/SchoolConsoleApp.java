58a92e0059c0482667886724ee83ea8a87a943b0
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2022-12-08 06:55:34 +0200

    run DB creation from console (before appl. start)
    create empty file create_tables.sql

--- SchoolConsoleApp/src/main/java/ua/foxminded/tarasevych/schoolconsoleapp/SchoolConsoleApp.java
+++ SchoolConsoleApp/src/main/java/ua/foxminded/tarasevych/schoolconsoleapp/SchoolConsoleApp.java
@@ -1,32 +1,24 @@
 package ua.foxminded.tarasevych.schoolconsoleapp;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
-import org.hsqldb.cmdline.SqlFile;
-import org.hsqldb.cmdline.SqlToolError;
 
 public class SchoolConsoleApp {
     static Logger logger = Logger.getLogger(SchoolConsoleApp.class);
 
-    public static void main(String[] args) throws SQLException, IOException, SqlToolError {
+    public static void main(String[] args) throws InterruptedException, IOException, URISyntaxException {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         PropertyConfigurator.configure(classLoader.getResource("log4j.properties "));
 
-        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/", "postgres",
-                "1234"); InputStream inputStream = classLoader.getResourceAsStream("script.sql")) {
-            SqlFile sqlFile = new SqlFile(new InputStreamReader(inputStream), "init", System.out, "UTF-8", false,
-                    new File("."));
-
-            sqlFile.setConnection(connection);
-            sqlFile.execute();
-        }
-
+        String command = "psql -U postgres -h localhost -p 5432 " + "-f "
+                + Paths.get(classLoader.getResource("initialScript.sql").toURI());
+        String[] envVars = { "PGPASSWORD=1234" };
+        Process runInitScript = Runtime.getRuntime().exec(command, envVars);
+        runInitScript.waitFor();
     }
 }
+
+
