6fe3e105534c58177d396155ece10492a9bbe7e6
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2022-12-10 07:20:13 +0200

    edit method generateCourses/class Course

--- SchoolConsoleApp/src/main/java/ua/foxminded/schoolconsoleapp/schooldata/Course.java
+++ SchoolConsoleApp/src/main/java/ua/foxminded/schoolconsoleapp/schooldata/Course.java
@@ -1,17 +1,13 @@
 package ua.foxminded.schoolconsoleapp.schooldata;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-import com.github.javafaker.Faker;
 
 public class Course {
 
     public List<String> generateCourses() {
-        Faker faker = new Faker();
-        return IntStream.range(0, 10).<String>mapToObj(i -> faker.educator().course())
-                .collect(Collectors.toCollection(() -> new ArrayList<>(10)));
+        return new ArrayList<>(Arrays.asList("Mathematics", "English", "Life Science", "Physical Science", "Geography",
+                "Computer Science", "History", "Sports", "Art", "Literature"));
     }
 }
