e3c935c425b2fede173abd3ef7150eef9250e6ed
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2022-12-11 07:14:36 +0200

    find a solution to generate unique students

--- SchoolConsoleApp/src/main/java/ua/foxminded/schoolconsoleapp/SchoolConsoleApp.java
+++ SchoolConsoleApp/src/main/java/ua/foxminded/schoolconsoleapp/SchoolConsoleApp.java
@@ -18,22 +18,27 @@ public class SchoolConsoleApp {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         PropertyConfigurator.configure(classLoader.getResource("log4j.properties "));
 
-        String command = "psql -U postgres -h localhost -p 5432 " + "-f "
-                + Paths.get(classLoader.getResource("initialScript.sql").toURI());
-        String[] envVars = { "PGPASSWORD=1234" };
-        Process runInitScript = Runtime.getRuntime().exec(command, envVars);
-        runInitScript.waitFor();
-
-        Tables.createTables("tables.sql");
-
-        Group group = new Group();
-        logger.info(group.generateGroups());
+//        String command = "psql -U postgres -h localhost -p 5432 " + "-f "
+//                + Paths.get(classLoader.getResource("initialScript.sql").toURI());
+//        String[] envVars = { "PGPASSWORD=1234" };
+//        Process runInitScript = Runtime.getRuntime().exec(command, envVars);
+//        runInitScript.waitFor();
+//
+//        Tables.createTables("tables.sql");
+//
+//        Group group = new Group();
+//        logger.info(group.generateGroups());
+//        
+//
+//        Course course = new Course();
+//        logger.info(course.generateCourses());
 
         Student student = new Student();
         logger.info(student.generateNames());
         logger.info(student.generateSurnames());
+        for (String s : student.generateStudents(student.generateNames(), student.generateSurnames())) {
+            logger.info(s);
+        }
 
-        Course course = new Course();
-        logger.info(course.generateCourses());
     }
 }
