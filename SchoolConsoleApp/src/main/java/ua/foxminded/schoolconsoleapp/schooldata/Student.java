0d5aa6eda152b4a3499d94a89c63ee121eec47ca
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2022-12-11 19:32:37 +0200

    - generate 20 first names and 20 last names using JavaFaker
    - randomly combine names and surnames to generate 200 students.

--- SchoolConsoleApp/src/main/java/ua/foxminded/schoolconsoleapp/schooldata/Student.java
+++ SchoolConsoleApp/src/main/java/ua/foxminded/schoolconsoleapp/schooldata/Student.java
@@ -4,64 +4,33 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import com.github.javafaker.Faker;
 
 public class Student {
+    Faker faker = new Faker();
 
     public List<String> generateNames() {
-        return new ArrayList<>(Arrays.asList("Dusty", "Drew", "Margie", "Marcus", "Quentin", "Glayds", "Naoma", "Eloy",
-                "Starla", "Holley", "Alida", "Dillon", "Alphonse", "Augusta", "Rudy", "Carole", "Dorsey", "Forrest",
-                "Oscar", "Harry"));
+        return IntStream.range(0, 20).<String>mapToObj(i -> faker.name().firstName())
+                .collect(Collectors.toCollection(() -> new ArrayList<>(20)));
     }
 
     public List<String> generateSurnames() {
-        return new ArrayList<>(Arrays.asList("MacGyver", "Gottlieb", "Bradtke", "Koelpin", "Krajcik", "Sawayn",
-                "Harvey", "Ankunding", "Dickens", "Lynch", "Stanton", "Wiegand", "Toy", " O'Reilly", "Skiles", "Mohran",
-                "Rutherford", "Lehner", "Hoeger", "Towne"));
+        return IntStream.range(0, 20).<String>mapToObj(i -> faker.name().lastName())
+                .collect(Collectors.toCollection(() -> new ArrayList<>(20)));
     }
 
     public Set<String> generateStudents(List<String> names, List<String> surnames) {
         Set<String> list = new HashSet<>();
-
         while (list.size() != 200) {
-            Collections.shuffle(names);
-            Collections.shuffle(surnames);
+            Arrays.asList(names, surnames).forEach(Collections::shuffle);
             Collections.addAll(list, names.get(0) + " " + surnames.get(0));
         }
         return list;
     }
-
-//    public Map<String, String> generateStudents(List<String> names, List<String> surnames) {
-//        Map<String, String> students = new LinkedHashMap<>();
-//
-//        return students;
-//    }
-
-//    Faker faker = new Faker();
-//
-//    public List<String> generateNames() {
-//        return IntStream.range(0, 20).<String>mapToObj(i -> faker.name().firstName())
-//                .collect(Collectors.toCollection(() -> new ArrayList<>(10)));
-//    }
-//
-//    public List<String> generateSurnames() {
-//        return IntStream.range(0, 20).<String>mapToObj(i -> faker.name().lastName())
-//                .collect(Collectors.toCollection(() -> new ArrayList<>(10)));
-//    }
-    
-    
-//    System.out.println("-----------------------------------------------------");
-//    Set<String> set = new HashSet<>();
-//    for (String name : list) {
-//        if (set.add(name) == false) {
-//            System.out.println(name);
-//        }
-//    }
-//
-//    System.out.println("-----------------------------------------------------");
 
 }
