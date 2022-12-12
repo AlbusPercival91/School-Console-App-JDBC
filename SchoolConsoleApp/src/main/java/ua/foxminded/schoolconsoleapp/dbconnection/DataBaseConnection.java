675cabe09fcbafbc08664efcee1790edb7afe7f7
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2022-12-12 03:34:01 +0200

    add class DataBaseConnection to simplify further queries
    rename class Tables to InitialTables
    
    rename some packages

---
+++ SchoolConsoleApp/src/main/java/ua/foxminded/schoolconsoleapp/dbconnection/DataBaseConnection.java
@@ -0,0 +1,22 @@
+package ua.foxminded.schoolconsoleapp.dbconnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseConnection {
+    static Connection connection;
+
+    private DataBaseConnection() {
+
+    }
+
+    public static Connection connect() {
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/school", "school_admin", "1234");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
--- SchoolConsoleApp/src/main/java/ua/foxminded/schoolconsoleapp/dbconnection/InitialTables.java
+++ SchoolConsoleApp/src/main/java/ua/foxminded/schoolconsoleapp/dbconnection/InitialTables.java
@@ -6,23 +6,21 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import org.apache.ibatis.jdbc.ScriptRunner;
 import org.apache.log4j.PropertyConfigurator;
 
-public class Tables {
+public class InitialTables {
 
-    private Tables() {
+    private InitialTables() {
 
     }
 
     public static void createTables(String fileName) {
-        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/school",
-                "school_admin", "1234");
-                InputStream inputStream = PropertyConfigurator.class.getResourceAsStream("/" + fileName)) {
+        try (Connection connection = DataBaseConnection.connect();
+                InputStream iStream = PropertyConfigurator.class.getResourceAsStream("/" + fileName)) {
             ScriptRunner runner = new ScriptRunner(connection);
-            Reader reader = new BufferedReader(new InputStreamReader(inputStream));
+            Reader reader = new BufferedReader(new InputStreamReader(iStream));
             runner.setLogWriter(null);
             runner.runScript(reader);
         } catch (IOException | SQLException e) {
