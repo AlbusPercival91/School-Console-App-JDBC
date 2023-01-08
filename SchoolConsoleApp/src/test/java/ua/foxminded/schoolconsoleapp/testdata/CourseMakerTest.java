4583c1d1590410bdfced1818c9d16ca7aa11e7c1
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-01-09 01:21:51 +0200

    refactor CourseMakerTest:
    - replace foreach in method
    assignCourseId_fromOneToThreePerStudentExpected() to Stream;

--- SchoolConsoleApp/src/test/java/ua/foxminded/schoolconsoleapp/testdata/CourseMakerTest.java
+++ SchoolConsoleApp/src/test/java/ua/foxminded/schoolconsoleapp/testdata/CourseMakerTest.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -32,14 +33,14 @@ class CourseMakerTest {
 
     static boolean assignCourseId_fromOneToThreePerStudentExpected() {
         CourseMaker course = new CourseMaker();
-        List<Integer> list = new ArrayList<>();
+        List<Integer> coursePerStudent = new ArrayList<>();
         boolean assigned = false;
 
-        for (Integer key : course.assignCourseId().keySet()) {
-            list.add(course.assignCourseId().get(key).size());
-        }
+        coursePerStudent = course.assignCourseId().keySet().stream().map(key -> course.assignCourseId().get(key).size())
+                .collect(Collectors.toList());
 
-        if (Collections.max(list) <= 3 && Collections.min(list) >= 1 && course.assignCourseId().size() == 200) {
+        if (Collections.max(coursePerStudent) <= 3 && Collections.min(coursePerStudent) >= 1
+                && course.assignCourseId().size() == 200) {
             assigned = true;
         }
         return assigned;
@@ -56,5 +57,4 @@ class CourseMakerTest {
     void coursesGenerationAndCoursesAssignation_ShouldBeEquals(Object expected, Object actual) {
         assertEquals(expected, actual);
     }
-
 }
