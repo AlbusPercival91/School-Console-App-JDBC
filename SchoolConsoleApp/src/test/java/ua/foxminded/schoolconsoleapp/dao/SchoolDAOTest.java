be2dcf3e3348756dadacd47c3130aa6f166f3bdb
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-01-15 23:28:07 +0200

    add test for method findStudentsRelatedToCourse()
    - create Stream<Arguments> findStudentsRelatedToCourse_CheckValues() in
    SchoolDAOTest;
    
    - make parameterized test
    findStudentsRelatedToCourse__ShouldBeMoreZero(boolean expected, boolean
    actual) in class SchoolDAOTest;

--- SchoolConsoleApp/src/test/java/ua/foxminded/schoolconsoleapp/dao/SchoolDAOTest.java
+++ SchoolConsoleApp/src/test/java/ua/foxminded/schoolconsoleapp/dao/SchoolDAOTest.java
@@ -5,12 +5,17 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import ua.foxminded.schoolconsoleapp.dbconnection.DBConnection;
 import ua.foxminded.schoolconsoleapp.dbconnection.ScriptReader;
 import ua.foxminded.schoolconsoleapp.dbconnection.TestConstants;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class SchoolDAOTest {
 
@@ -37,6 +42,79 @@ class SchoolDAOTest {
         int matchedPattern = (int) actual.stream().map(pattern::matcher).filter(Matcher::find).count();
 
         assertEquals(10, matchedPattern);
+    }
+
+    static Stream<Arguments> findStudentsRelatedToCourse_CheckValues() {
+        ScriptReader.readSqlScript(TestConstants.STARTUP_SCRIPT,
+                DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""));
+        TestDataDAO testData = new TestDataDAO();
+        testData.createGroup(DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""));
+        testData.createCourse(DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""));
+        testData.createStudent(DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""));
+        testData.createCourseStudentRelation(DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""));
+        return Stream.of(
+                arguments(true,
+                        (int) SchoolDAO
+                                .findStudentsRelatedToCourse("History",
+                                        DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""))
+                                .stream().filter(s -> s.trim().contains(" ")).count() > 0),
+                arguments(true,
+                        SchoolDAO
+                                .findStudentsRelatedToCourse("Literature",
+                                        DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""))
+                                .stream().filter(s -> s.trim().contains(" ")).count() > 0),
+                arguments(true,
+                        SchoolDAO
+                                .findStudentsRelatedToCourse("Computer Science",
+                                        DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""))
+                                .stream().filter(s -> s.trim().contains(" ")).count() > 0),
+                arguments(true,
+                        SchoolDAO
+                                .findStudentsRelatedToCourse("Geography",
+                                        DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""))
+                                .stream().filter(s -> s.trim().contains(" ")).count() > 0),
+                arguments(true,
+                        SchoolDAO
+                                .findStudentsRelatedToCourse("Physical Science",
+                                        DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""))
+                                .stream().filter(s -> s.trim().contains(" ")).count() > 0),
+                arguments(true,
+                        SchoolDAO
+                                .findStudentsRelatedToCourse("Life Science",
+                                        DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""))
+                                .stream().filter(s -> s.trim().contains(" ")).count() > 0),
+                arguments(true,
+                        SchoolDAO
+                                .findStudentsRelatedToCourse("English",
+                                        DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""))
+                                .stream().filter(s -> s.trim().contains(" ")).count() > 0),
+                arguments(true,
+                        SchoolDAO
+                                .findStudentsRelatedToCourse("Mathematics",
+                                        DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""))
+                                .stream().filter(s -> s.trim().contains(" ")).count() > 0),
+                arguments(true,
+                        SchoolDAO
+                                .findStudentsRelatedToCourse("Sports",
+                                        DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""))
+                                .stream().filter(s -> s.trim().contains(" ")).count() > 0),
+                arguments(true,
+                        SchoolDAO
+                                .findStudentsRelatedToCourse("Art",
+                                        DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""))
+                                .stream().filter(s -> s.trim().contains(" ")).count() > 0));
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("ua.foxminded.schoolconsoleapp.dao.SchoolDAOTest#findStudentsRelatedToCourse_CheckValues()")
+    void findStudentsRelatedToCourse__ShouldBeMoreZero(boolean expected, boolean actual) {
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void addNewStudent() {
+
     }
 
 }
