11008ef80f8a8cfafe0449b05f0848aa8d05f82e
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-01-16 17:21:45 +0200

    completedaddStudentToTheCourse_StringExpectedAndActual_ShouldBeEquals
    - add method findStudentsRelatedToCourse_CheckValues that return stream
    of values to be checked in parameterized test
    
    - create parameterized test method
    completedaddStudentToTheCourse_StringExpectedAndActual_ShouldBeEquals
    where check equality of expected and actual Strings when assigning
    student to course

--- SchoolConsoleApp/src/test/java/ua/foxminded/schoolconsoleapp/dao/SchoolDAOTest.java
+++ SchoolConsoleApp/src/test/java/ua/foxminded/schoolconsoleapp/dao/SchoolDAOTest.java
@@ -59,7 +59,7 @@ class SchoolDAOTest {
         testData.createCourseStudentRelation(DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""));
         return Stream.of(
                 arguments(true,
-                        (int) SchoolDAO
+                        SchoolDAO
                                 .findStudentsRelatedToCourse("History",
                                         DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""))
                                 .stream().filter(s -> s.trim().contains(" ")).count() > 0),
@@ -150,6 +150,52 @@ class SchoolDAOTest {
                     DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""));
         }
         assertEquals(1, deleted);
+    }
+
+    static Stream<Arguments> addStudentToTheCourse_StringExpectedAndActual() {
+        ScriptReader.readSqlScript(TestConstants.STARTUP_SCRIPT,
+                DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""));
+        TestDataDAO testData = new TestDataDAO();
+        testData.createGroup(DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""));
+        testData.createCourse(DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""));
+        testData.createStudent(DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""));
+        testData.createCourseStudentRelation(DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""));
+        return Stream.of(
+                arguments("Student with ID: 12 assigned to course: Art",
+                        SchoolDAO.addStudentToTheCourse(12, "Art",
+                                DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""))),
+                arguments("Student with ID: 190 assigned to course: Literature",
+                        SchoolDAO.addStudentToTheCourse(190, "Literature",
+                                DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""))),
+                arguments("Student with ID: 19 assigned to course: Computer Science",
+                        SchoolDAO.addStudentToTheCourse(19, "Computer Science",
+                                DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""))),
+                arguments("Student with ID: 21 assigned to course: Geography",
+                        SchoolDAO.addStudentToTheCourse(21, "Geography",
+                                DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""))),
+                arguments("Student with ID: 193 assigned to course: Physical Science",
+                        SchoolDAO.addStudentToTheCourse(193, "Physical Science",
+                                DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""))),
+                arguments("Student with ID: 1 assigned to course: Life Science",
+                        SchoolDAO.addStudentToTheCourse(1, "Life Science",
+                                DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""))),
+                arguments("Student with ID: 9 assigned to course: English",
+                        SchoolDAO.addStudentToTheCourse(9, "English",
+                                DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""))),
+                arguments("Student with ID: 2 assigned to course: Mathematics",
+                        SchoolDAO.addStudentToTheCourse(2, "Mathematics",
+                                DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""))),
+                arguments("Student with ID: 150 assigned to course: Sports",
+                        SchoolDAO.addStudentToTheCourse(150, "Sports",
+                                DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""))),
+                arguments("Student with ID: 7 assigned to course: History", SchoolDAO.addStudentToTheCourse(7,
+                        "History", DBConnection.getConnection("jdbc:h2:~/test;MODE=PostgreSQL", "", ""))));
+    }
+
+    @ParameterizedTest
+    @MethodSource("ua.foxminded.schoolconsoleapp.dao.SchoolDAOTest#addStudentToTheCourse_StringExpectedAndActual()")
+    void addStudentToTheCourse_StringExpectedAndActual_ShouldBeEquals(String expected, String actual) {
+        assertEquals(expected, actual);
     }
 
 }
