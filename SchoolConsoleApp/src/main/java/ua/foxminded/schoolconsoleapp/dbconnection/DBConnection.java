ccc882de773cd718720b086e15173e7a16e1956b
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-01-19 23:13:58 +0200

    change getPsqlConnection()
    - create method getHomeDir that looking for home directory
    - create method readProperties that read file properties
    - create final variable File file with constructor parameters
    (getHomeDir(), link to file)
    - now getPsqlConnection() take file with db settings from file created
    in os directory and run connection

--- SchoolConsoleApp/src/main/java/ua/foxminded/schoolconsoleapp/dbconnection/DBConnection.java
+++ SchoolConsoleApp/src/main/java/ua/foxminded/schoolconsoleapp/dbconnection/DBConnection.java
@@ -1,19 +1,47 @@
 package ua.foxminded.schoolconsoleapp.dbconnection;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnection {
     static Connection connection;
+    private static final File PROPS = new File(getHomeDir(), "config" + File.separator + "db.properties");
 
     private DBConnection() {
 
     }
 
+    private static File getHomeDir() {
+        String prop = System.getProperty("user.home");
+        File homeDir = new File(prop == null ? "." : prop);
+
+        if (!homeDir.isDirectory()) {
+            throw new IllegalStateException(homeDir + " is not directory");
+        }
+        return homeDir;
+    }
+
+    private static Properties readProperties() {
+        Properties props = new Properties();
+
+        try (InputStream is = new FileInputStream(PROPS)) {
+            props.load(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return props;
+    }
+
     public static Connection getPsqlConnection() {
         try {
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/school", "school_admin", "1234");
+            connection = DriverManager.getConnection(readProperties().getProperty("db.url"),
+                    readProperties().getProperty("db.login"), readProperties().getProperty("db.password"));
         } catch (SQLException e) {
             throw new IllegalArgumentException("Wrong connection parameters");
         }
